dd6e877ced0a3d18069a2de3eb13dbd9363c0998
SuelenGalhardo <suelenranucci@gmail.com>
2024-02-08 22:55:42 +0100

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 
+import logo from "./assets/logo-nlw-expert.svg"
+
+
 //export en el inicio , para tener que poner llaves en la inportacion de main
 export function App() {
   
   return (
-    
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
 
-  
-  
+    <><img src={logo} alt="logo NLW/expert" />
+    <form className="w-full">
+    <input className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500" type="text" placeholder="busque em suas notas" /></>
+    </form>
+    <div className=" h-px bg-slate-700"></div>
+
+    <div className=" grid grid-cols-3 auto-rows-[250px]">
+      <div className=" rounded-md bg-slate-780"></div>
+
+
+    </div>
+    </div>
+    
 );
 }
 
