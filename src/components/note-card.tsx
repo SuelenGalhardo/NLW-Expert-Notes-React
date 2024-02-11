31528d53ef5f98bfa8040d9bcc4f7f7d577439bf
SuelenGalhardo <suelenranucci@gmail.com>
2024-02-11 08:14:17 +0100

    added changes in note card

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,7 +1,16 @@
-export function NoteCard() {
+
+interface NoteCardProps {
+  date: Date
+  content: string
+}
+
+
+export function NoteCard(props: NoteCardProps) {
+
+ 
     return (
-      <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
-        <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+      <button className="rounded-md text-left flex-col bg-slate-800 gap-3 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+        <span className="text-sm font-medium text-slate-300">{props.date.toISOString()}</span>
   
         <p className="text-sm leading-6 text-slate-400">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus commodi
